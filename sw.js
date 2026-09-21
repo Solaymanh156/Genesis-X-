3803b1d2363b825baa19d2baf50dc14c6d56625e
Solaymanh156 <solaymanh156@gmail.com>
2026-09-22 01:14:52 +0600

    Add service worker for asset caching
    Implement service worker for caching assets.

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'genesis-x-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+// Service Worker Install
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Fetching Assets
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
